57bb8c91df6a7e7ca3135e92172e5d6536c8d8d1
Juan Fabritzzio Pescoran Angulo <118696670+JuanPescoran@users.noreply.github.com>
2025-10-05 16:03:32 -0500

    feat: add all modules to start working with microservices
    for more information in how the structure was make, follow the next documentation: https://spring.io/guides/gs/multi-module

---
+++ db-init-scripts/01-init-databases.sql
@@ -0,0 +1,4 @@
+﻿-- Solo creamos las bases de datos para los futuros servicios.
+-- 'identity_db' es creada automáticamente por las variables de entorno de Docker.
+CREATE DATABASE IF NOT EXISTS batch_management_db;
+CREATE DATABASE IF NOT EXISTS traceability_db;
