162e9690b75b6c584017bc56115dd2e7a59fb1ae
Juan Fabritzzio Pescoran Angulo <118696670+JuanPescoran@users.noreply.github.com>
2025-10-05 17:04:54 -0500

    chore: add batch management initializr

---
+++ batch-management-context/src/test/java/com/foodchain/batch_management_context/BatchManagementContextApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodchain.batch_management_context;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchManagementContextApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
