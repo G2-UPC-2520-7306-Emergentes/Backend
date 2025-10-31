71eff8084943a84e6837bcdfc0d73733e6f130f2
Juan Fabritzzio Pescoran Angulo <118696670+JuanPescoran@users.noreply.github.com>
2025-10-31 03:35:48 -0500

    feat: add shared-infrastructure

---
+++ shared-infrastructure/src/test/java/com/foodchain/shared_infrastructure/SharedInfrastructureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodchain.shared_infrastructure;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SharedInfrastructureApplicationTests {
+
+	@Test
+    public void contextLoads() {
+	}
+
+}
