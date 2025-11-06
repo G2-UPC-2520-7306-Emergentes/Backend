c38e13ca43d9b8b543971134d393240dafa334af
Juan Fabritzzio Pescoran Angulo <118696670+JuanPescoran@users.noreply.github.com>
2025-11-06 16:05:44 -0500

    fix!: remove all boms and add configuration build

--- identity-context/src/main/java/com/foodchain/identity_context/domain/model/valueobjects/ERole.java
+++ identity-context/src/main/java/com/foodchain/identity_context/domain/model/valueobjects/ERole.java
@@ -1,13 +1,11 @@
-﻿// domain/model/valueobjects/ERole.java
+// domain/model/valueobjects/ERole.java
 package com.foodchain.identity_context.domain.model.valueobjects;
 
 /**
  * VALUE OBJECT: ERole (Enum)
- *
  * Define un conjunto fijo y seguro de los nombres de roles disponibles en todo el sistema.
  * Esto actúa como el "contrato" de dominio para los roles, asegurando que solo se puedan
  * usar valores válidos y predefinidos.
- *
  * Sigue la convención de Spring Security de prefijar los roles con "ROLE_".
  */
 public enum ERole {
